c3dab63e147dfa719c98361e5c08d8322476d5c8
ox1234 <d4y1ightl@gmail.com>
2022-08-13 11:42:06 +0800

    feat(banlist): add ban list

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"houseMonitor/log"
 	"houseMonitor/notify"
 	"houseMonitor/notify/template"
 	douban2 "houseMonitor/site/douban"
+	"io/ioutil"
+	"net/http"
 	"strings"
 	"time"
 )
@@ -15,10 +18,12 @@ func main() {
 	var appID string
 	var appSecret string
 	var chatName string
+	var conf string
 
 	flag.StringVar(&appID, "a", "", "app id")
 	flag.StringVar(&appSecret, "s", "", "app secret")
 	flag.StringVar(&chatName, "c", "", "chat name")
+	flag.StringVar(&conf, "conf", "", "config file")
 
 	flag.Parse()
 
@@ -29,6 +34,24 @@ func main() {
 
 	lk := notify.NewLarkNotify(appID, appSecret, chatName)
 	douban := douban2.NewDouBanCollector()
+
+	banWords, err := getBanList(conf)
+	if err != nil {
+		log.Error("get ban list fail: %s", err)
+		return
+	}
+
+	go func() {
+		for range time.Tick(time.Second * 30) {
+			log.Info("start get ban list...")
+			banWords, err = getBanList(conf)
+			if err != nil {
+				log.Error("cron get ban list fail: %s", err)
+				continue
+			}
+			log.Info("refresh ban list with: %+v", banWords)
+		}
+	}()
 
 	log.Info("start house monitor....")
 	for range time.Tick(time.Minute * 10) {
@@ -42,6 +65,11 @@ func main() {
 		if len(newItems) > 0 {
 			var msgs []string
 			for _, item := range newItems {
+				for _, banWord := range banWords {
+					if strings.Contains(item.Description, banWord) {
+						continue
+					}
+				}
 				msgs = append(msgs, item.String())
 			}
 
@@ -53,4 +81,22 @@ func main() {
 		}
 	}
 
+}
+
+func getBanList(u string) ([]string, error) {
+	resp, err := http.Get(u)
+	if err != nil {
+		return nil, fmt.Errorf("get ban list fail: %w", err)
+	}
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("get body fail: %w", err)
+	}
+	var banList []string
+	err = json.Unmarshal(b, &banList)
+	if err != nil {
+		return nil, fmt.Errorf("unmarshal body fail: %w", err)
+	}
+	return banList, nil
 }
